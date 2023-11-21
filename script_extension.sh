548c6ff4e10cd2f3dd9bd969e77b36b39d73222e
Youness-Allous-Sopra <131863262+Youness-Allous-Sopra@users.noreply.github.com>
2023-11-21 14:07:53 +0100

    Create script_extension.sh

---
+++ script_extension.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+rpm -Uvh https://repo.zabbix.com/zabbix/6.4/rhel/8/x86_64/zabbix-release-6.4-1.el8.noarch.rpm
+dnf clean all
+dnf install zabbix-agent -y
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
+reboot
+# zabbix_agentd -V   #permet de verifier la version de l'agent
